5cb024f72a00e76818ef9dae70cd44c59b673647
Trofimov Vladyslav <suzirie@gmail.com>
2017-09-08 17:51:32 +0300

    continue

--- js/app.js
+++ js/app.js
@@ -1,13 +1,51 @@
-var App = React.createClass({
+let	my_news	=	[
+    {
+        author:	'Саша	Печкин',
+        text:	'В	четверг,	четвертого	числа...'
+    },
+    {
+        author:	'Просто	Вася',
+        text:	'Считаю,	что	$	должен	стоить	35	рублей!'
+    },
+    {
+        author:	'Гость',
+        text:	'Бесплатно.	Скачать.	Лучший	сайт	-	http://localhost:3000'
+    }
+];
+
+
+
+let App = React.createClass({
     render:function () {
         return (
             <div className="app">
                 React component
+                <News data={my_news}/>
+                <Comments />
             </div>
         );
     }
 });
 
+let News = React.createClass({
+   render:function () {
+       return (
+         <div className="news">
+             There are no news:(
+         </div>
+       );
+   }
+});
+
+let Comments = React.createClass({
+    render:function () {
+        return (
+            <div className="comments">
+                no comments
+            </div>
+        );
+    }
+});
 
 ReactDOM.render(
     <App />,
