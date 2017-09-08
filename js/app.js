7fc7b45c784f9c8230989be90064ccb926628c85
Trofimov Vladyslav <suzirie@gmail.com>
2017-09-08 17:30:04 +0300

    start

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var App = React.createClass({
+    render:function () {
+        return (
+            <div className="app">
+                React component
+            </div>
+        );
+    }
+});
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
